d0f5731c9e055f3925252c5d2c0ca9428158ed50
Carolina Soler-Rodriguez <casoler11@gmail.com>
2015-09-26 14:37:12 -0500

    add title functionality for project show page

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -6,10 +6,15 @@ RSpec.feature 'Users can create new projects' do
 
     click_link 'New Project'
 
-    fill_in 'Name', with: 'Sublime Text 3'
+    fill_in 'Name', with: 'Sublime Text 2'
     fill_in 'Description', with: 'A text editor for everyone'
     click_button 'Create Project'
 
     expect(page).to have_content 'Project has been created.'
+
+    project = Project.find_by(name: 'Sublime Text 2')
+    expect(page.current_url).to eq project_url(project)
+    title = 'Sublime Text 2 - Projects - Ticketee'
+    expect(page).to have_title title
   end
 end
